da5d055b816f3b207f29301748e6f558a12772ee
Abhivanya <your_email@example.com>
2024-11-29 22:05:10 +0530

    adding list form

--- src/components/meals/mealsItem/MealItem.jsx
+++ src/components/meals/mealsItem/MealItem.jsx
@@ -3,9 +3,19 @@ import Style from "./MealItem.module.css";
 const MealItem = ({ item }) => {
   return (
     <li key={item.id} className={Style.item}>
-      <div className={Style.title}>{item.title}</div>
-      <div className={Style.description}>{item.description}</div>
-      <div className={Style.price}>${item.price}</div>
+      <div>
+        <div className={Style.title}>{item.title}</div>
+        <div className={Style.description}>{item.description}</div>
+        <div className={Style.price}>${item.price}</div>
+      </div>
+
+      <form>
+        <div className={Style.input}>
+          <label htmlFor="amount">Amount</label>
+          <input type="number" min={1} max={5} defaultValue={1} />
+        </div>
+        <button className={Style.btn}>+ Add</button>
+      </form>
     </li>
   );
 };
